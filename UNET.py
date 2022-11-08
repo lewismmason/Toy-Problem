ef361a907e6f5a96489da5906189364e7351067a
Lewis <lewis.montgomery.mason@gmail.com>
2022-11-08 12:50:51 -0800

    playing around with a 1D NN that takes an input of lenght length and matches it to 3 classes. Data is obviously made up

--- UNET.py
+++ UNET.py
@@ -1,12 +1,37 @@
 # Testing for the network stuff
 
-inputs = keras.Input(shape=(784,))
-inputs.shape
+def main():
+    length = 122
+
+    input = keras.Input(shape=(length,))
+    x = layers.Dense(30, activation='relu')(input)
+    output = layers.Dense(3,activation='relu')(x)
+
+    model = keras.Model(input, output, name='test')
+    model.compile(optimizer = keras.optimizers.Adam(learning_rate = 1e-3),
+            loss = keras.losses.BinaryCrossentropy())
+    model.summary()
+
+    num_samples = 20
+
+    data_in = np.random.random(size=[num_samples,length])
+    data_val = keras.utils.to_categorical(np.random.randint(3,size=num_samples))
+
+    history = model.fit(data_in, data_val, validation_split=0.2,
+                epochs = 1000, batch_size = 64)
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['training loss','val loss'])
+    plt.show()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import numpy as np
     import tensorflow as tf
-    from tensorflow.python import keras
-    from tensorflow.python.keras import layers
+    import keras
+    from keras import layers
+    import matplotlib.pyplot as plt
+    main()
