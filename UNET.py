4359be77504188fbbbbd60ed15f066167cae69b4
Lewis <lewis.montgomery.mason@gmail.com>
2022-11-08 12:11:15 -0800

    made some minor changes, I don't remember what but they are insignificant

---
+++ UNET.py
@@ -0,0 +1,12 @@
+# Testing for the network stuff
+
+inputs = keras.Input(shape=(784,))
+inputs.shape
+
+
+
+if __name__ == '__main__':
+    import numpy as np
+    import tensorflow as tf
+    from tensorflow.python import keras
+    from tensorflow.python.keras import layers
